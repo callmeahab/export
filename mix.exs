540e819d0e06d48c1cc67c501857dd66f9ab7d86
Milos Markovic <milos@imrepublic.com>
2018-07-29 18:01:37 +0200

    Using erlport branch with OTP 21 support.

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Export.Mixfile do
 
   defp deps() do
     [
-      {:erlport, "~> 0.9"},
+      {:erlport, github: "callmeahab/erlport"},
       {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
